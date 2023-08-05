c3f49021629e01c4ba1b25195c4652896550ea2a
Sanghel González <sanghel.gonzalez@walibre.com>
2023-08-05 00:27:14 -0400

    code email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend"
 import { validateString, getErrorMessage } from "@/lib/utils"
 import ContactFormEmail from "@/email/contactFormEmail"
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend('re_GnRbyzNk_P7BYDzL2bpNMyZ3QhSZwNYEw')
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail")
