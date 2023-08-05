d0dc0f0d841491e73eca3cc880da14ca506f3262
Sanghel González <sanghel.gonzalez@walibre.com>
2023-08-05 01:03:32 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,14 @@ import ActiveSectionContextProvider from '@/context/activeSectionContext'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 import ThemeSwitch from '@/components/themeSwitch'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Sanghel | Portfolio',
-  description: 'Sanghel, Front-end developer with +1 years of experience.',
-}
+// export const metadata = {
+//   title: 'Sanghel | Portfolio',
+//   description: 'Sanghel, Front-end developer with +1 years of experience.',
+// }
 
 export default function RootLayout({
   children,
@@ -21,6 +22,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <head>
+        <title>Sanghel | Portfolio</title>
+        <meta name="description" content="Sanghel, Front-end developer with +1 years of experience." />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={`bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90  ${inter.className}`}>
         <div className="bg-[#F27EDF] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] opacity-90 dark:bg-[#c759b5"></div>
         <div className="bg-[#6f23D9] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-2rem] opacity-50 dark:bg-[#370f6f]"></div>
@@ -33,7 +45,6 @@ export default function RootLayout({
           </ActiveSectionContextProvider>
           <ThemeSwitch />
         </ThemeContextProvider>
-        
       </body>
     </html>
   )
